06bf72e08541582d98e95fdd373d607d58f7218d
HyeonJeong Lim <applepie0526@naver.com>
2023-02-06 22:35:28 +0900

    Fix: change source code after code review

--- week2/sesam0526/BOJ1021.java
+++ week2/sesam0526/BOJ1021.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ1021 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         LinkedList<Integer> deque = new LinkedList<>();
         StringTokenizer st = new StringTokenizer(in.readLine(), " ");
@@ -10,33 +10,32 @@ public class BOJ1021 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        for(int i=1; i<=N; i++)
+        for (int i = 1; i <= N; i++)
             deque.offer(i);
         int[] seq = new int[M];
 
         st = new StringTokenizer(in.readLine(), " ");
-        for(int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++) {
             seq[i] = Integer.parseInt(st.nextToken());
         }
 
         int count = 0;
+        int half_idx;
 
-        for(int i=0; i<M; i++){
+        if (deque.size() % 2 == 0)
+            half_idx = deque.size() / 2 - 1;
+        else half_idx = deque.size() / 2;
+
+        for (int i = 0; i < M; i++) {
             int target_idx = deque.indexOf(seq[i]);
-            int half_idx;
 
-            if(deque.size() % 2 == 0)
-                half_idx = deque.size() / 2 - 1 ;
-            else half_idx = deque.size() / 2;
-
-            if(target_idx <= half_idx){
-                for(int j=0; j<target_idx; j++){
+            if (target_idx <= half_idx) {
+                for (int j = 0; j < target_idx; j++) {
                     deque.offerLast(deque.pollFirst());
                     count++;
                 }
-            }
-            else{
-                for(int j=0; j<deque.size()-target_idx; j++){
+            } else {
+                for (int j = 0; j < deque.size() - target_idx; j++) {
                     deque.offerFirst(deque.pollLast());
                     count++;
                 }
--- week2/sesam0526/BOJ2812.java
+++ week2/sesam0526/BOJ2812.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ2812 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(in.readLine());
         StringBuilder sb = new StringBuilder();
@@ -14,16 +14,16 @@ public class BOJ2812 {
 
         String s = in.readLine();
         int count = 0;
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             int n = s.charAt(i) - '0';
 
-            while(!stack.isEmpty() && K > count && stack.peek() < n){
+            while (!stack.isEmpty() && K > count && stack.peek() < n) {
                 stack.pop();
                 count++;
             }
             stack.push(n);
         }
-        for(int i=0; i<N-K; i++)
+        for (int i = 0; i < N - K; i++)
             sb.append(stack.get(i));
         System.out.print(sb);
     }
